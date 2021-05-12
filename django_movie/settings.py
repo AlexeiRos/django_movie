fbd042f097a9045edaed271fd421353e9ea1d8c8
AlexeiRos <rostokin.s@mail.ru>
2021-05-12 17:45:15 +0300

    up

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -163,10 +163,11 @@ LOCALE_PATHS = (os.path.join(os.path.dirname(__file__), '../locale/'),)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+# STATICFILES_DIRS = [STATIC_DIR]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
