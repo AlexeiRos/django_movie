c132566f2951a89fb86b239c83f234ad1c3048ed
AlexeiRos <rostokin.s@mail.ru>
2021-05-12 18:53:39 +0300

    up

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -166,9 +166,11 @@ LOCALE_PATHS = (os.path.join(os.path.dirname(__file__), '../locale/'),)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATICFILES_DIRS = [STATIC_DIR]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
