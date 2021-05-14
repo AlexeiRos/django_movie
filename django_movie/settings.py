6817d71e619360dd6571c766a2abfe2eac185d62
AlexeiRos <rostokin.s@mail.ru>
2021-05-14 16:39:42 +0300

    test fix

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from django.conf.urls.static import static
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import django_heroku
 
