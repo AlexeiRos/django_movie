07dd351b4fa5fac962317ff9ccc5d4c690dd588f
AlexeiRos <rostokin.s@mail.ru>
2021-05-15 16:19:32 +0300

    fix

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -164,6 +164,9 @@ LOCALE_PATHS = (os.path.join(os.path.dirname(__file__), '../locale/'),)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
