43bbbae395769de7215b38ef02e3300a2cd82955
AlexeiRos <rostokin.s@mail.ru>
2021-05-15 16:28:28 +0300

    fix

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     path('accounts/', include('allauth.urls')),
